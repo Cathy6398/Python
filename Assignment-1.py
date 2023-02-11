4d577ea7011b6511d33e2bf24d5cec2bfc89fc0a
Cathy6398 <59562672+Cathy6398@users.noreply.github.com>
2023-02-11 16:55:27 +0530

    Add files via upload
    Hello
    Addition :  12
    Division :  2.0
    Area of Triangle :  6.0
    Before swap A: 8 B: 9
    After swap A: 9 B: 8
    Randon Number: 562
    PS C:\Users\kirub>

---
+++ Assignment-1.py
@@ -0,0 +1,29 @@
+### 1. Write a Python program to print "Hello Python"?
+print("Hello");
+
+### 2. Write a Python program to do arithmetical operations addition and division.?
+a=8 
+b= 4
+add= a+b
+div=a/b
+print("Addition : ",add)
+print("Division : ",div);
+
+### 3. Write a Python program to find the area of a triangle?
+height=4
+base = 3
+area = ((height*base)/2)
+print("Area of Triangle : ",area);
+
+### 4. Write a Python program to swap two variables?
+a=8
+b=9
+print("Before swap A:",a,"B:",b);
+temp =a
+a=b
+b=temp
+print("After swap A:",a,"B:",b);
+
+### 5. Write a Python program to generate a random number?
+import random
+print(random.randint(1,1000));
