1c4f654324d64f934c7fd07699ef8fc8d1bc69f3
Cathy6398 <59562672+Cathy6398@users.noreply.github.com>
2023-02-11 18:46:31 +0530

    Add files via upload

---
+++ Assignment-3.py
@@ -0,0 +1,22 @@
+### 1. Write a Python Program to Check if a Number is Positive, Negative or Zero?
+a=int(input("Enter the Number to Check if a Number is Positive, Negative or Zero: "))
+if(a > 0):
+   print(a,"is Positive")
+elif(a < 0):
+   print(a,"is Negative")
+else:
+   print(a,"is Zero")
+
+### 2. Write a Python Program to Check if a Number is Odd or Even?
+n=int(input("Enter the Number to Check if a Number is Odd or Even: "))
+if(n % 2 ==0):
+  print(n,"is Even")
+else:
+    print(n,"is odd")
+
+### 3. Write a Python Program to Check Leap Year?
+year = int(input("Enter a number to Check Leap Year: "))
+if(year % 4 == 0):
+    print(year,"its Leap Year")
+else:
+   print(year,"its Not Leap Year")
