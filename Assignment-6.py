bd1bd15f8e81a4717a559b94b42838d16241cf1d
Cathy6398 <59562672+Cathy6398@users.noreply.github.com>
2023-02-12 22:47:15 +0530

    Add files via upload

---
+++ Assignment-6.py
@@ -0,0 +1,44 @@
+### 1. Write a Python Program to Display Fibonacci Sequence Using Recursion?
+def fibo(a,b,c):
+    if c > 0:
+        c -= 1
+        print(a, end=' ')
+        temp = b
+        b = a + b
+        a = temp
+        fibo(a,b,c)
+fibo(0,1,10)
+
+### 2. Write a Python Program to Find Factorial of Number Using Recursion?
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num=int(input("\nEnter the number : "))
+print("The factorial of", num, "is", factorial(num))
+
+### 3. Write a Python Program to calculate your Body Mass Index?
+def bmi(height, weight):
+    
+    calc=weight/(height*height)
+    return calc
+
+w=int(input("Enter the weight in Kg : "))
+h=int(input("Enter the height in meters : "))
+
+print("The BMI is ", bmi(h,w))
+
+### 4. Write a Python Program to calculate the natural logarithm of any number?
+import math
+num = int(input("\nEnter the number to calculate the natural logarithm: "))
+print(math.log(num))
+
+### 5. Write a Python Program for cube sum of first n natural numbers?
+def cub(n):
+    return sum(range(n+1))**3
+
+s=int(input("\nEnter the number to  cube sum of first n natural numbers"))
+print("Result is ",cub(s))
