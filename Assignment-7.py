d9412991fa04562091aae7aede40dff34e6eea4d
Cathy6398 <59562672+Cathy6398@users.noreply.github.com>
2023-02-18 19:47:06 +0530

    Add files via upload

---
+++ Assignment-7.py
@@ -0,0 +1,34 @@
+### 1. Write a Python Program to find sum of array?
+l = [1,2,3,-412, 123, 369, 111]
+arraySum = 0
+for i in l:
+    arraySum += i  
+print("1.Sum of array: ", arraySum)
+
+### 2. Write a Python Program to find largest element in an array?
+arrayMax = 1000
+for i in l:
+    if i > arrayMax:
+        arrayMax = i
+print("2.",arrayMax)
+
+### 3. Write a Python Program for array rotation?
+l = [1,2,3,-412, 123, 369, 111]
+print("3.Rotated array:",l[::-1])
+
+### 4. Write a Python Program to Split the array and add the first part to the end?
+def split(arr, k):
+    arr = arr[k:] + arr[:k]
+    return arr 
+k = 2
+arr = [10, 13, 5, 17]
+print("4.Output array is", split(arr, k))
+
+### 5. Write a Python Program to check if given array is Monotonic?
+
+def isMonotonic(A):
+   return (all(A[i] <= A[i + 1] for i in range(len(A) - 1)) or
+      all(A[i] >= A[i + 1] for i in range(len(A) - 1)))
+# main
+A = [8,1,2,3,4,7,8]
+print("5.",isMonotonic(A))
