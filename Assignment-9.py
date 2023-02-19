b6d186d45ff7f16427cd24fb0626c6e736296510
Cathy6398 <59562672+Cathy6398@users.noreply.github.com>
2023-02-19 13:56:10 +0530

    Update Assignment-9.py

--- Assignment-9.py
+++ Assignment-9.py
@@ -74,3 +74,49 @@ for i in range(1,101):
         happyNo.append(i)
 print(happyNo)
 
+### 5. Write a Python program to determine whether the given number is a Harshad Number?
+def digsum(num):
+    sum = 0
+    while(num>0):
+        rem = num%10
+        sum= sum + rem      
+        num = num//10
+    return sum
+
+num = int(input("5.Enter the number : "))
+sum = digsum(num)
+
+if num % sum == 0:
+    print(num,"Is a Harshad number")
+else:
+    print(num,"Is not a Harshad number")
+
+### 6. Write a Python program to print all pronic numbers between 1 and 100?
+def PronicNo(num):
+    Pronic = False
+    for i in range(1,num+1):
+        if i*(i+1) == num:
+            Pronic = True
+            break
+    return Pronic
+
+print("6.Pronic numbers in range 1 to 100")
+pronicno = []
+for i in range(1,101):
+    if PronicNo(i):
+        pronicno.append(i)
+print(pronicno)
+
+Solution
+1.Enter the Number : 175
+Disarium number
+2.Disarium no in range 1 to 100
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+3.Enter the number :19
+19 Is a Happy Number
+4.Happy Number no in range 1 to 100
+[1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97, 100]
+5.Enter the number : 21
+21 Is a Harshad number
+6.Pronic numbers in range 1 to 100
+[2, 6, 12, 20, 30, 42, 56, 72, 90]
